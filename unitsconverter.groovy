734bcf86261bbc5117d35b65e9e166de73a0501c
leechuck2@googlemail.com <leechuck2@googlemail.com@6fe5be6e-fe62-a92d-8738-6cd99b028d12>
2011-04-07 08:24:35 +0000

    generate multiple versions of uo.owl

--- unitsconverter.groovy
+++ unitsconverter.groovy
@@ -55,13 +55,21 @@ infile.eachLine {
 def onturi = "http://purl.obolibrary.org/obo/"
 OWLOntologyManager man = OWLManager.createOWLOntologyManager();
 OWLDataFactory fac = man.getOWLDataFactory()
-OWLOntology ont = man.createOntology(IRI.create(onturi+"uo.owl"))
+OWLOntology ont = man.createOntology(IRI.create(onturi+"uo.owl")) // full version
+OWLOntology ont2 = man.createOntology(IRI.create(onturi+"uo2.owl")) // without instances
+OWLOntology ont3 = man.createOntology(IRI.create(onturi+"uo3.owl")) // without singleton defs
+OWLOntology ont4 = man.createOntology(IRI.create(onturi+"uo4.owl")) // without units as classes
 def unitof = fac.getOWLObjectProperty(IRI.create(onturi+"unit_of"))
 
 l.each {
   def cls = onturi+(it.id.replaceAll(":","_"))
   def cl = fac.getOWLClass(IRI.create(cls))
   man.addAxiom(ont, fac.getOWLDeclarationAxiom(cl))
+  man.addAxiom(ont2, fac.getOWLDeclarationAxiom(cl))
+  man.addAxiom(ont3, fac.getOWLDeclarationAxiom(cl))
+  if (!it.unit) {
+    man.addAxiom(ont4, fac.getOWLDeclarationAxiom(cl))
+  }
   if (it.unit) {
     //    def cls2 = onturi+"i"+it.id
     def cls2 = onturi+(it.id.replaceAll(":","_"))
@@ -69,12 +77,20 @@ l.each {
     def oneof = fac.getOWLObjectOneOf(ind)
     def equiv = fac.getOWLEquivalentClassesAxiom(cl,oneof)
     man.addAxiom(ont, equiv)
+    equiv = fac.getOWLClassAssertionAxiom(cl, ind)
+    man.addAxiom(ont3, equiv)
   }
   it.isa.each { sup ->
     sup = sup.replaceAll(":","_")
     def cl2 = fac.getOWLClass(IRI.create(onturi+sup))
     def subc = fac.getOWLSubClassOfAxiom(cl,cl2)
     man.addAxiom(ont, subc)
+    man.addAxiom(ont2, subc)
+    man.addAxiom(ont3, subc)
+    def cls2 = onturi+(it.id.replaceAll(":","_"))
+    def ind = fac.getOWLNamedIndividual(IRI.create(cls2))
+    def equiv = fac.getOWLClassAssertionAxiom(cl2, ind)
+    man.addAxiom(ont4, equiv)
   }
   def label = fac.getRDFSLabel()
   def definition = fac.getRDFSComment()
@@ -85,6 +101,9 @@ l.each {
     anno = fac.getOWLAnnotation(definition, fac.getOWLTypedLiteral(it.definition))
     annoassert = fac.getOWLAnnotationAssertionAxiom(IRI.create(cls),anno)
     man.addAxiom(ont,annoassert)
+    man.addAxiom(ont2,annoassert)
+    man.addAxiom(ont3,annoassert)
+    man.addAxiom(ont4,annoassert)
   }
   if (it.rel.size()>0) {
     it.rel = it.rel.collect { fac.getOWLClass(IRI.create(patouri+it.replaceAll(":","_"))) }
@@ -94,15 +113,25 @@ l.each {
     cl2 = fac.getOWLObjectAllValuesFrom(unitof, cl2)
     def subc = fac.getOWLSubClassOfAxiom(cl,cl2)
     man.addAxiom(ont, subc)
+    man.addAxiom(ont2, subc)
+    man.addAxiom(ont3, subc)
+    man.addAxiom(ont4, subc)
 
   }
 }
 
 File f = new File("uo.owl")
 man.saveOntology(ont, IRI.create("file:"+f.getCanonicalFile()))
+f = new File("uo-without-instances.owl")
+man.saveOntology(ont2, IRI.create("file:"+f.getCanonicalFile()))
+f = new File("uo-without-singleton-definitions.owl")
+man.saveOntology(ont3, IRI.create("file:"+f.getCanonicalFile()))
+f = new File("uo-without-units-as-classes.owl")
+man.saveOntology(ont4, IRI.create("file:"+f.getCanonicalFile()))
 
 f = new File("uo-with-pato.owl")
 def imp = fac.getOWLImportsDeclaration(IRI.create("http://www.berkeleybop.org/ontologies/obo-all/quality/quality.owl2"))
 AddImport ai = new AddImport(ont, imp)
 man.applyChange(ai)
 man.saveOntology(ont, IRI.create("file:"+f.getCanonicalFile()))
+
